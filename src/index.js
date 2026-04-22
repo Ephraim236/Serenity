bc5079408997c81d29bfa1eb62fde84f68117f09
Ephra <your.email@example.com>
2026-04-22 20:25:25 +0000

    New Update

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ try {
 
 // Middleware
 app.use(cors({
-  origin: ['https://serenity-frontend-5.onrender.com', 'http://localhost:5173', 'http://localhost:5000'],
+  origin: ['https://serenity-frontend-phi.vercel.app', 'http://localhost:5173', 'http://localhost:5000'],
   credentials: true
 }));
 app.use(express.json());
